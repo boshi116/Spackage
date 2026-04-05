5cf5cdcc22f3e6652aa7b9a16e75b9da3637b4b3
boshi116 <qiboshi1980@gmail.com>
2026-04-05 14:55:39 +0800

    Update xunlei.lua

--- opd/luci-app-xunlei/luasrc/controller/xunlei.lua
+++ opd/luci-app-xunlei/luasrc/controller/xunlei.lua
@@ -2,6 +2,6 @@
 module("luci.controller.xunlei", package.seeall)
 
 function index()
-  entry({"admin", "services", "xunlei"}, alias("admin", "services", "xunlei", "config"), _("Xunlei"), 30).dependent = true
+  entry({"admin", "services", "xunlei"}, alias("admin", "services", "xunlei", "config"), _("Xunlei"), 630).dependent = true
   entry({"admin", "services", "xunlei", "config"}, cbi("xunlei"))
 end
