5050268e85619e9024cdc4435481acd84bc83167
boshi116 <qiboshi1980@gmail.com>
2026-04-05 14:54:47 +0800

    Update unishare.lua

--- opd/luci-app-unishare/luasrc/controller/unishare.lua
+++ opd/luci-app-unishare/luasrc/controller/unishare.lua
@@ -7,7 +7,7 @@ function index()
 		return
 	end
 
-	entry({"admin", "nas", "unishare"}, alias("admin", "nas", "unishare", "index"), _("UniShare"), 2).dependent = true
+	entry({"admin", "nas", "unishare"}, alias("admin", "nas", "unishare", "index"), _("UniShare"), 600).dependent = true
 	entry({"admin", "nas", "unishare", "index"}, cbi("unishare/index"), _("UniShare"), 1)
 	entry({"admin", "nas", "unishare", "users"}, cbi("unishare/users"), _("Users"), 2)
 	entry({"admin", "nas", "unishare", "share"}, cbi("unishare/share", {hideapplybtn=true})).leaf = true
